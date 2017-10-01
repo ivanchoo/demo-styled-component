b0c9f64ba70b54d2ddc50be16bc7ad1603c83d94
Ivan Choo <ivanchoo.private@gmail.com>
2017-10-01 11:35:00 +0800

    Ditching sass for styled-components 💅

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,35 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 const PORTAL_MIN_WIDTH = 768; // small devices, landscape
 const USER_COLUMN_WIDTH = 280;
+
+const Container = styled.div`
+  background-color: gray;
+  position: absolute;
+  top: 0; left: 0; right: 0; bottom: 0;
+`;
 
 export default class App extends React.Component {
   render() {
     const { store } = this.props;
     return (
-      <div className="d-flex" style={styles.portal}>
+      <Container>
         <code>TODO</code>
-      </div>
+      </Container>
 
     );
   }
 }
-
-const styles = {
-  portal: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    minWidth: PORTAL_MIN_WIDTH
-  },
-  usersContainer: {
-    flex: 1,
-    overflowY: "auto",
-    overflowX: "hidden"
-  },
-  sideColumn: {
-    width: USER_COLUMN_WIDTH
-  }
-};
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,45 +1,45 @@
 /* eslint-disable */
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
-var isProduction = process.env.NODE_ENV === 'production';
+var isProduction = process.env.NODE_ENV === "production";
 
 var r = path.resolve;
 var BASE_DIR = r(__dirname);
-var BUILD_DIR = r(BASE_DIR, 'build');
-var SRC_DIR = r(BASE_DIR, 'src');
-var CONTENTBASE_DIR = r(BASE_DIR, 'html');
+var BUILD_DIR = r(BASE_DIR, "build");
+var SRC_DIR = r(BASE_DIR, "src");
+var CONTENTBASE_DIR = r(BASE_DIR, "html");
 
 var entrypoint = function(source) {
   if (isProduction) {
     return source;
   }
   return [
-    'react-hot-loader/patch',
-    'webpack/hot/only-dev-server',
-    'webpack-dev-server/client?http://0.0.0.0:3000',
+    "react-hot-loader/patch",
+    "webpack/hot/only-dev-server",
+    "webpack-dev-server/client?http://0.0.0.0:3000"
   ].concat(source);
-}
+};
 
 module.exports = {
   entry: {
     index: entrypoint([
-      'babel-polyfill',
-      'isomorphic-fetch',
-      r(SRC_DIR, 'index.js'),
-    ]),
+      "babel-polyfill",
+      r(SRC_DIR, "styles", "reboot.css"),
+      r(SRC_DIR, "index.js")
+    ])
   },
   output: {
     path: BUILD_DIR,
-    publicPath: '/lib/',
-    filename: '[name].bundle.js',
-    library: '[name]',
-    libraryTarget: 'var'
+    publicPath: "/lib/",
+    filename: "[name].bundle.js",
+    library: "[name]",
+    libraryTarget: "var"
   },
   devServer: {
     contentBase: CONTENTBASE_DIR,
     disableHostCheck: true,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
     hot: true,
     inline: true,
@@ -49,23 +49,23 @@ module.exports = {
     }
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: [".js", ".jsx", ".json"]
   },
-  devtool: isProduction ? 'cheap-module-source-map' : 'eval',
+  devtool: isProduction ? "cheap-module-source-map" : "eval",
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-         NODE_ENV: JSON.stringify(isProduction ? 'production' : 'development')
-       }
-    }),
+      "process.env": {
+        NODE_ENV: JSON.stringify(isProduction ? "production" : "development")
+      }
+    })
   ],
   module: {
     loaders: [
-      { test: /\.(jsx|js)$/, loader: 'babel-loader', include: SRC_DIR },
-      { test: /\.gif$/, loader: "url-loader?limit=1000&mimetype=image/gif" }
+      { test: /\.(jsx|js)$/, loader: "babel-loader", include: SRC_DIR },
+      { test: /\.css$/, use: ["style-loader", "css-loader"], include: SRC_DIR }
     ]
   }
-}
+};
